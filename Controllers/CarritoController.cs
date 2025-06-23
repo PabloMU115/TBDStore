aab83b83c3c5a49ffaa65307ee5ac6fb15a5326d
Pablo Mora <pablomu199@gmail.com>
2025-06-23 07:36:06 -0600

    Creación del controlador de carrito de compras, index del carrito, el cual representa la versión inicial de la vista del carrito.

---
+++ Controllers/CarritoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TBD.Data;
+using TBD.Models;
+using TBD.Models.ViewModels;
+
+namespace TBD.Controllers
+{
+    public class CarritoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Usuario> _userManager;
+
+        public CarritoController(ApplicationDbContext context, UserManager<Usuario> userManager) 
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [Route("/cart")]
+        public ActionResult Index()
+        {
+            var idUsuario = _userManager.GetUserId(User);
+            var items = (from c in _context.Carrito where c.IdUsuario.Equals(idUsuario) join
+                     p in _context.Productos on c.IdProducto equals p.IdProducto
+                     
+                     select new CarritoViewModel 
+                     {
+                         IdProducto = p.IdProducto,
+                         NombreProducto = p.NombreProducto,
+                         StockProducto = p.StockDisponible,
+                         PrecioProducto = p.Precio,
+                         ImagenProducto = p.ImagenUrl,
+                         CantidadProducto = c.Cantidad
+                     }).ToList();
+            return View(items);
+        }
+    }
+}
---
+++ Models/ViewModels/CarritoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TBD.Models.ViewModels
+{
+    public class CarritoViewModel
+    {
+        public string IdProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public decimal PrecioProducto { get; set; }
+        public int StockProducto { get; set; }
+        public string ImagenProducto { get; set; }
+        public int CantidadProducto { get; set; }
+    }
+}
